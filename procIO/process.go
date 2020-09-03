09e734cc0d0cb7c0a10bc91c64c433945a65cd1a
Luca Corbatto <luca@corbatto.de>
2020-09-03 14:50:05 +0200

    Adds CachingProcess and uses it by default

--- procIO/process.go
+++ procIO/process.go
@@ -14,6 +14,48 @@ type Process interface {
 	MemorySegments() ([]*MemorySegmentInfo, error)
 }
 
-func OpenProcess(pid int) (Process, error) {
-	return open(pid)
+type CachingProcess interface {
+	Process
+	InvalidateCache()
+}
+
+func OpenProcess(pid int) (CachingProcess, error) {
+	proc, err := open(pid)
+	return &cachingProcess{
+		proc:  proc,
+		cache: nil,
+	}, err
+}
+
+type cachingProcess struct {
+	proc  Process
+	cache []*MemorySegmentInfo
+}
+
+func (c cachingProcess) Close() error {
+	return c.proc.Close()
+}
+
+func (c cachingProcess) String() string {
+	return c.proc.String()
+}
+
+func (c cachingProcess) PID() int {
+	return c.proc.PID()
+}
+
+func (c cachingProcess) Handle() interface{} {
+	return c.proc.Handle()
+}
+
+func (c cachingProcess) MemorySegments() ([]*MemorySegmentInfo, error) {
+	var err error
+	if c.cache == nil {
+		c.cache, err = c.proc.MemorySegments()
+	}
+	return c.cache, err
+}
+
+func (c cachingProcess) InvalidateCache() {
+	c.cache = nil
 }
