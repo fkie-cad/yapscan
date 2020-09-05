4964ac3621ccd7c6d1439cf58a218bf224a7361c
Luca Corbatto <luca@corbatto.de>
2020-09-05 12:08:49 +0200

    Overhauls docker build system

---
+++ buildForWindows.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+go get github.com/abice/go-enum
+go generate ./...
+mkdir -p build/ &>/dev/null
+
+docker build --network=host -t yapscan-xcompile -f Dockerfile.xwin .
+
+docker run --rm --network=host --volume $(pwd):/opt/yapscan -i yapscan-xcompile <<EOF
+export CC=x86_64-w64-mingw32-gcc
+export LD=x86_64-w64-mingw32-ld
+export CGO_ENABLED=1
+export GOOS=windows
+export CGO_CFLAGS="-I/opt/yara/libyara/include"
+export CGO_LDFLAGS="-L/opt/yara/libyara/.libs -lyara -static"
+
+cd yapscan/cmd/yapscan
+go build -o /opt/yapscan/build/yapscan.exe -tags yara_no_pkg_config
+EOF
