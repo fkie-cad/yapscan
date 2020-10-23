b065e5f5484d35f8a3a8bd8045005b36ee75f03b
Luca Corbatto <luca@corbatto.de>
2020-10-23 11:18:08 +0200

    Makes Concurrent and Concat nicer to use

--- fileIO/iterator.go
+++ fileIO/iterator.go
@@ -17,11 +17,39 @@ type concatIterator struct {
 	iterators []Iterator
 }
 
-func Concat(iterators ...Iterator) Iterator {
+func concat(it1 Iterator, it2 Iterator) Iterator {
+	if it1 == nil {
+		return it2
+	}
+	if it2 == nil {
+		return it1
+	}
+	cit1, ok1 := it1.(*concatIterator)
+	cit2, ok2 := it2.(*concatIterator)
+	if ok1 && ok2 {
+		cit1.iterators = append(cit1.iterators, cit2.iterators...)
+		return cit1
+	}
+	if ok1 {
+		cit1.iterators = append(cit1.iterators, it2)
+		return cit1
+	}
+	if ok2 {
+		cit2.iterators = append(cit2.iterators, it1)
+		return cit2
+	}
 	return &concatIterator{
 		i:         0,
-		iterators: iterators,
+		iterators: []Iterator{it1, it2},
 	}
+}
+
+func Concat(iterators ...Iterator) Iterator {
+	var ret Iterator
+	for _, it := range iterators {
+		ret = concat(ret, it)
+	}
+	return ret
 }
 
 func (it *concatIterator) Next() (*File, error) {
@@ -52,6 +80,47 @@ type concurrentIterator struct {
 	closed    bool
 }
 
+func concurrent(it1 Iterator, it2 Iterator) Iterator {
+	if it1 == nil {
+		return it2
+	}
+	if it2 == nil {
+		return it1
+	}
+	cit1, ok1 := it1.(*concurrentIterator)
+	cit2, ok2 := it2.(*concurrentIterator)
+	if ok1 && ok2 {
+		panic("cannot combine two concurrent iterators")
+	}
+	if ok1 {
+		cit1.iterators = append(cit1.iterators, it2)
+		cit1.wg.Add(1)
+		go cit1.consume(len(cit1.iterators) - 1)
+		return cit1
+	}
+	if ok2 {
+		cit2.iterators = append(cit2.iterators, it1)
+		cit2.wg.Add(1)
+		go cit2.consume(len(cit2.iterators) - 1)
+		return cit2
+	}
+
+	cit := &concurrentIterator{
+		iterators: []Iterator{it1, it2},
+		c:         make(chan *nextEntry),
+		wg:        new(sync.WaitGroup),
+	}
+	cit.wg.Add(2)
+	go cit.consume(0)
+	go cit.consume(1)
+
+	go func() {
+		cit.wg.Wait()
+		close(cit.c)
+	}()
+	return cit
+}
+
 func Concurrent(iterators ...Iterator) Iterator {
 	it := &concurrentIterator{
 		iterators: iterators,
@@ -64,18 +133,11 @@ func Concurrent(iterators ...Iterator) Iterator {
 		go it.consume(i)
 	}
 
-	go func() {
-		it.wg.Wait()
-		close(it.c)
-	}()
-
 	return it
 }
 
 func (it *concurrentIterator) consume(i int) {
-	defer func() {
-		it.wg.Done()
-	}()
+	defer it.wg.Done()
 
 	for {
 		f, err := it.iterators[i].Next()
