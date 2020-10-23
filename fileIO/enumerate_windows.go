1c4a54800c35fd020e2849f28f3cc2e74a61c461
Luca Corbatto <luca@corbatto.de>
2020-10-23 14:50:15 +0200

    Added workaround for windows UAC hiding network shares

--- fileIO/enumerate_windows.go
+++ fileIO/enumerate_windows.go
@@ -1,10 +1,85 @@
 package fileIO
 
 import (
+	"context"
 	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/targodan/go-errors"
+
+	"github.com/sirupsen/logrus"
 
 	"golang.org/x/sys/windows"
 )
+
+var (
+	netShareInitialTimeout = 1 * time.Second
+	netShareMountTimeout   = 1 * time.Second
+)
+
+func makeSharesAvailableAsAdmin() error {
+	netUseOutput, err := func() (string, error) {
+		ctx, cancel := context.WithTimeout(context.Background(), netShareInitialTimeout)
+		defer cancel()
+
+		netUse := exec.CommandContext(ctx, "net", "use")
+		output, err := netUse.Output()
+		return string(output), err
+	}()
+	if err != nil {
+		return err
+	}
+
+	// Example output (tested on windows 7 ultimate x64):
+	//PS C:\users\user\Desktop> net use
+	//New connections will be remembered.
+	//
+	//
+	//Status       Local     Remote                    Network
+	//
+	//-------------------------------------------------------------------------------
+	//Unavailable  Z:        \\192.168.xxx.xxx\share   Microsoft Windows Network
+	//The command completed successfully.
+	lineRe := regexp.MustCompile(`^(\S+)\s+(\S+)\s+(\S+)\s+.+$`)
+
+	unavailable := make(map[string]string, 0)
+
+	lines := strings.Split(netUseOutput, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+		matches := lineRe.FindAllStringSubmatch(line, -1)
+		if len(matches) == 1 && len(matches[0]) == 4 {
+			status, local, remote := matches[0][1], matches[0][2], matches[0][3]
+			if status == "Unavailable" {
+				unavailable[local] = remote
+			}
+		}
+	}
+
+	err = nil
+	for drive, remote := range unavailable {
+		logrus.Infof("Attempting to make drive \"%s\" available to current user.", drive)
+
+		tmpErr := func() error {
+			ctx, cancel := context.WithTimeout(context.Background(), netShareMountTimeout)
+			defer cancel()
+
+			netUse := exec.CommandContext(ctx, "net", "use", drive, remote)
+			return netUse.Run()
+		}()
+		if tmpErr != nil {
+			logrus.WithError(err).Errorf("Could not make drive \"%s\" available to current user.", drive)
+
+			err = errors.NewMultiError(err, tmpErr)
+		} else {
+			logrus.Errorf("Successfully made drive \"%s\" available to current user.", drive)
+		}
+	}
+	return err
+}
 
 func nativeToGoDriveType(t uint32) DriveType {
 	switch t {
@@ -55,6 +130,8 @@ func loadAllDrives() []*drive {
 }
 
 func enumerateImpl(typeMask DriveType) ([]string, error) {
+	err := makeSharesAvailableAsAdmin()
+
 	if allDrives == nil {
 		allDrives = loadAllDrives()
 	}
@@ -66,5 +143,5 @@ func enumerateImpl(typeMask DriveType) ([]string, error) {
 		}
 	}
 
-	return ret, nil
+	return ret, err
 }
