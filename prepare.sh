fc71e5cfd368cd24189f296618f0d0797a10ded5
Luca Corbatto <luca@corbatto.de>
2021-01-08 09:33:05 +0100

    Adds optional update flag for tools

--- prepare.sh
+++ prepare.sh
@@ -2,11 +2,16 @@
 
 cd $(dirname "$0") || exit 1
 
+update=""
+if [[ "$1" == "-u" ]]; then
+    update="-u"
+fi
+
 go mod tidy
 go mod vendor
 
-go get -v github.com/abice/go-enum
-go get github.com/vektra/mockery/v2/.../
+go get -v $update github.com/abice/go-enum
+go get -v $update github.com/vektra/mockery/v2/.../
 go mod tidy
 
 go generate ./...
