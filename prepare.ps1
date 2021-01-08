680defc2a9b9ecb3abf435b8bf0197d2b23e7cb8
Luca Corbatto <luca@corbatto.de>
2021-01-08 09:34:22 +0100

    Makes updating tools optional

--- prepare.ps1
+++ prepare.ps1
@@ -1,6 +1,16 @@
+Param(
+    [Parameter(Mandatory=$False)]
+    [switch]$UpdateTools
+)
+
 # PowerShell <3.0 compatibility
 if ($PSScriptRoot -like "") {
     $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+}
+
+$update=""
+if ($UpdateTools) {
+    $update="-u"
 }
 
 Push-Location $PSScriptRoot
@@ -8,8 +18,8 @@ Push-Location $PSScriptRoot
 go mod tidy
 go mod vendor
 
-go get -v -u github.com/abice/go-enum
-go get -v -u github.com/vektra/mockery/v2/.../
+go get -v $update github.com/abice/go-enum
+go get -v $update github.com/vektra/mockery/v2/.../
 go mod tidy
 
 go generate ./...
