440b1971a3344b6be580a59723850e964c6cfb64
Luca Corbatto <luca@corbatto.de>
2021-05-14 12:19:45 +0200

    Makes compiled rule tests yara version agnostic

--- yara_test.go
+++ yara_test.go
@@ -2,10 +2,15 @@ package yapscan
 
 import (
 	"errors"
+	"io"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/yeka/zip"
 
 	"github.com/hillu/go-yara/v4"
 
@@ -139,8 +144,8 @@ func TestLoadYaraRules(t *testing.T) {
 		})
 	})
 
-	Convey("Loading a single compiled yara rule", t, func() {
-		rules, err := LoadYaraRules(testDataDir("rules_compiled", "rule1and2.yarc"), false)
+	Convey("Loading a single compiled yara rule", t, withCompiledRules1And2(func(rulesPath string) {
+		rules, err := LoadYaraRules(rulesPath, false)
 		So(rules, ShouldNotBeNil)
 		So(err, ShouldBeNil)
 
@@ -155,7 +160,7 @@ func TestLoadYaraRules(t *testing.T) {
 			})
 			So(len(names), ShouldEqual, 2)
 		})
-	})
+	}))
 
 	Convey("Loading multiple uncompiled yara rules", t, func() {
 		Convey("non-recursively", func() {
@@ -214,8 +219,8 @@ func TestLoadYaraRules(t *testing.T) {
 			})
 		})
 
-		Convey("with compiled yara rules", func() {
-			rules, err := LoadYaraRules(testDataDir("rules_zipped", "compiled.zip"), true)
+		Convey("with compiled yara rules", withCompiledAndZippedRules1And2And3(func(rulesPath string) {
+			rules, err := LoadYaraRules(rulesPath, true)
 			So(rules, ShouldNotBeNil)
 			So(err, ShouldBeNil)
 
@@ -230,6 +235,116 @@ func TestLoadYaraRules(t *testing.T) {
 				})
 				So(len(names), ShouldEqual, 3)
 			})
-		})
+		}))
 	})
+}
+
+func withCompiledRules1And2(inner func(rulesPath string)) func(c C) {
+	return func(c C) {
+		r1, err := ioutil.ReadFile(testDataDir("rules_uncompiled", "rule1.yara"))
+		if err != nil {
+			panic(err)
+		}
+		r2, err := ioutil.ReadFile(testDataDir("rules_uncompiled", "rule2.yara"))
+		if err != nil {
+			panic(err)
+		}
+
+		sb := &strings.Builder{}
+		sb.Write(r1)
+		sb.WriteString("\n")
+		sb.Write(r2)
+
+		rules := yara.MustCompile(sb.String(), nil)
+
+		tmpFile, err := ioutil.TempFile(os.TempDir(), "rule1and2*.yarc")
+		if err != nil {
+			panic(err)
+		}
+		rulesPath := tmpFile.Name()
+		tmpFile.Close()
+
+		err = rules.Save(rulesPath)
+		if err != nil {
+			panic(err)
+		}
+
+		c.Reset(func() {
+			os.ReadFile(rulesPath)
+		})
+
+		inner(rulesPath)
+	}
+}
+
+func withCompiledAndZippedRules1And2And3(inner func(rulesPath string)) func(c C) {
+	return func(c C) {
+		r1, err := ioutil.ReadFile(testDataDir("rules_uncompiled", "rule1.yara"))
+		if err != nil {
+			panic(err)
+		}
+		r2, err := ioutil.ReadFile(testDataDir("rules_uncompiled", "rule2.yara"))
+		if err != nil {
+			panic(err)
+		}
+		r3, err := ioutil.ReadFile(testDataDir("rules_uncompiled", "subdir", "rule3.yara"))
+		if err != nil {
+			panic(err)
+		}
+
+		sb := &strings.Builder{}
+		sb.Write(r1)
+		sb.WriteString("\n")
+		sb.Write(r2)
+		sb.WriteString("\n")
+		sb.Write(r3)
+
+		rules := yara.MustCompile(sb.String(), nil)
+
+		tmpFile, err := ioutil.TempFile(os.TempDir(), "rule1and2and3*.yarc")
+		if err != nil {
+			panic(err)
+		}
+		compiledRulesPath := tmpFile.Name()
+		tmpFile.Close()
+
+		err = rules.Save(compiledRulesPath)
+		if err != nil {
+			panic(err)
+		}
+
+		rulesPath := func() string {
+			compiled, err := os.Open(compiledRulesPath)
+			if err != nil {
+				panic(err)
+			}
+			defer compiled.Close()
+
+			tmpFile, err = ioutil.TempFile(os.TempDir(), "rule1and2and3*.zip")
+			if err != nil {
+				panic(err)
+			}
+			defer tmpFile.Close()
+
+			z := zip.NewWriter(tmpFile)
+			defer z.Close()
+
+			out, err := z.Encrypt("rules.yarc", RulesZIPPassword, zip.AES256Encryption)
+			if err != nil {
+				panic(err)
+			}
+			_, err = io.Copy(out, compiled)
+			if err != nil {
+				panic(err)
+			}
+			return tmpFile.Name()
+		}()
+
+		c.Reset(func() {
+			os.ReadFile(compiledRulesPath)
+			os.ReadFile(rulesPath)
+		})
+
+		inner(rulesPath)
+	}
 }
