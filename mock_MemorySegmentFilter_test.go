f89f20e638604a4d054a72c2a934b0f0abbd096f
Luca Corbatto <luca.corbatto@fkie.fraunhofer.de>
2021-06-22 13:58:08 +0200

    Updates mock

--- mock_MemorySegmentFilter_test.go
+++ mock_MemorySegmentFilter_test.go
@@ -12,6 +12,20 @@ type MockMemorySegmentFilter struct {
 	mock.Mock
 }
 
+// Description provides a mock function with given fields:
+func (_m *MockMemorySegmentFilter) Description() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	return r0
+}
+
 // Filter provides a mock function with given fields: info
 func (_m *MockMemorySegmentFilter) Filter(info *procio.MemorySegmentInfo) *FilterMatch {
 	ret := _m.Called(info)
