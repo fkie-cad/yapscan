f98a01a4fd7f897084251771d0eff4e0b58af1ef
Luca Corbatto <luca.corbatto@fkie.fraunhofer.de>
2021-07-14 11:01:39 +0200

    Bumps version

--- app/app.go
+++ app/app.go
@@ -209,7 +209,7 @@ func MakeApp(args []string) *cli.App {
 		Name:        "yapscan",
 		HelpName:    "yapscan",
 		Description: "A yara based scanner for files and process memory with some extras.",
-		Version:     "0.9.0",
+		Version:     "0.10.0",
 		Writer:      os.Stdout,
 		ErrWriter:   os.Stderr,
 		Authors: []*cli.Author{
