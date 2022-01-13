0d2290508bc2eaa0923436c342f020c9595c0cd3
Luca Corbatto <luca.corbatto@fkie.fraunhofer.de>
2022-01-13 15:15:02 +0100

    Adds output for as-service
    Related to #15

--- app/asService.go
+++ app/asService.go
@@ -1,10 +1,17 @@
 package app
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/fkie-cad/yapscan/service/output"
 
 	"github.com/urfave/cli/v2"
 )
@@ -24,11 +31,7 @@ func asService(args []string) error {
 		return cli.Exit(fmt.Sprintf("ERROR: Could not determine absolute path of yapscan.exe, %v\n", err), 1)
 	}
 
-	scStartArguments := []string{"start", serviceName}
-	scStartArguments = append(scStartArguments, args...)
-
 	fmt.Println("WARNING: This feature is experimental!")
-	fmt.Println("WARNING: You will not see any output of the executed service. Using --log-path is strongly advised.")
 
 	fmt.Println("Removing service in case it exists already...")
 	err = run("sc.exe", "delete", "yapscan")
@@ -45,6 +48,57 @@ func asService(args []string) error {
 	}
 	fmt.Println("Done installing service.")
 
+	fmt.Println("Starting output proxy...")
+	proxy := output.NewOutputProxyServer()
+	err = proxy.Listen()
+	defer proxy.Close()
+
+	outputReadyForConnection := &sync.WaitGroup{}
+	connectionSuccess := false
+	waitConnection := &sync.WaitGroup{}
+
+	if err != nil {
+		proxy = nil
+		fmt.Printf("WARNING: Could not start output proxy, the service will still be started, but no output will be visible. Reason: %v\n", err)
+	} else {
+		outputReadyForConnection.Add(1)
+		waitConnection.Add(1)
+		go func() {
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+
+			defer func() {
+				waitConnection.Done()
+			}()
+
+			outputReadyForConnection.Done()
+			var err error
+			err = proxy.WaitForConnection(ctx)
+
+			if errors.Is(ctx.Err(), context.DeadlineExceeded) {
+				fmt.Println("WARNING: Output proxy connection timed out. The service was likely still started, but no output will be visible.")
+				return
+			}
+			if err != nil {
+				fmt.Printf("WARNING: Service did not connect to output proxy. The service was likely still started, but no output will be visible. Reason: %v\n", err)
+				return
+			}
+
+			connectionSuccess = true
+		}()
+		fmt.Println("Done, proxy is waiting for connections.")
+	}
+
+	scStartArguments := []string{"start", serviceName}
+	if proxy != nil {
+		scStartArguments = append(scStartArguments, strconv.Itoa(proxy.StdoutPort()), strconv.Itoa(proxy.StderrPort()))
+	} else {
+		scStartArguments = append(scStartArguments, "0", "0")
+	}
+	scStartArguments = append(scStartArguments, args...)
+
+	outputReadyForConnection.Wait()
+
 	fmt.Println("Starting service with arguments...")
 	err = run("sc.exe", scStartArguments...)
 	if err != nil {
@@ -52,5 +106,31 @@ func asService(args []string) error {
 	}
 	fmt.Println("Done starting service, yapscan should now be running as a service with the following arguments")
 	fmt.Println(args)
+
+	if proxy != nil {
+		waitConnection.Wait()
+		if !connectionSuccess {
+			return nil
+		}
+
+		fmt.Println()
+		fmt.Println("========== Yapscan Service Output ==========")
+
+		outputDone := &sync.WaitGroup{}
+		outputDone.Add(1)
+		go func() {
+			var err error
+			defer func() {
+				outputDone.Done()
+			}()
+
+			err = proxy.ReceiveAndOutput(context.Background(), os.Stdout, os.Stderr)
+			if err != nil {
+				fmt.Printf("WARNING: Output proxy connection broke. The service might still be running, but you will not see any further output. Reason: %v\n", err)
+			}
+		}()
+		outputDone.Wait()
+	}
+
 	return nil
 }
